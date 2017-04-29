ba72490730316d27fc2950f25e67cb20fa9d97c9
ambouh <aphricanboi@gmail.com>
2017-04-29 02:32:21 -0400

    added routes to create post, get all or each post, and updating upvotes for each post

--- routes/index.js
+++ routes/index.js
@@ -33,3 +33,40 @@ router.post('/posts', function(req, res, next) {
     res.json(post);
   });
 });
+
+/*Rather than replicating the same code across several different request handler functions,
+we can use Express's param() function to automatically load an object.
+- when a route URL with :post requested, this function will run first (because of the query interface)
+*/
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  /*mongoose's query interface provides a flexible way of interacting with db*/
+  query.exec(function (err, post) {
+    if(err){return next(err);}
+    if (!post) {return next(new Error('Can\'t find post'));}
+
+    //- once the post with ID is retrieved, it's attached to the 'req' object
+    req.post = post;
+
+    //I think returning next() chains the next function to run
+    return next();
+  });
+});
+
+/*using the route URL /posts/:post, the param() function triggers first to query the json with id provided
+* - param() executes the next function
+* - */
+router.get('/posts/:post', function (req, res) {
+   res.json(req.post);
+});
+
+
+/*allows us to upvote a post */
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function (err, post) {
+    if(err) {return next(err);}
+
+    res.json(post);
+  });
+});
